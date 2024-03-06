0f1d20b15ab6a5125a09a414d22f8af7f920b07e
Cary Leanne <162423623+josebabbel@users.noreply.github.com>
2024-03-06 10:17:12 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,29 @@
+var	fromIndex = columns.indexOf(from),
+		toIndex, rows = [];
+		
+		if (fromIndex > -1) {
+			
+			columns.splice(fromIndex, 1);
+			
+			toIndex = columns.indexOf(to);
+			
+			if (toIndex > -1) {
+				
+				if(after){
+					toIndex = toIndex+1;
+				}
+				
+			}else{
+				toIndex = fromIndex;
+			}
+      
+			if(typeof position === "undefined"){
+				position = this.table.options.scrollToColumnPosition;
+			}
+			
+			if(typeof ifVisible === "undefined"){
+				ifVisible = this.table.options.scrollToColumnIfVisible;
+			}
+			
+			if(column.visible){
+				
